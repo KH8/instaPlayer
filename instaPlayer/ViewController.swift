daf95de1f94f16122245c596a4eec01084ebad75
kh8 <reczek.krzysztof@gmail.com>
2018-01-30 23:23:53 +0100

    Image recognition

--- instaPlayer/ViewController.swift
+++ instaPlayer/ViewController.swift
@@ -16,12 +16,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func takePhoto(_ sender: Any) {
@@ -36,7 +34,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            myImg.contentMode = .scaleToFill
+            myImg.contentMode = .scaleAspectFill
             myImg.image = pickedImage
         }
         picker.dismiss(animated: true, completion: nil)
@@ -45,10 +43,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func checkPicture(_ sender: Any) {
         let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=" + AppConstants.googleApiKey)
         
+        let imageData:Data =  UIImageJPEGRepresentation(myImg.image!, CGFloat(AppConstants.imageCompression))!
+        let base64String = imageData.base64EncodedString()
+        
+        print(UIImagePNGRepresentation(myImg.image!)!.base64EncodedString().lengthOfBytes(using: String.Encoding.utf8))
+        print(base64String.lengthOfBytes(using: String.Encoding.utf8))
+        
+        let body = "{\"requests\":[{\"image\":{\"content\":\"" + base64String + "\"},\"features\":[{\"type\":\"WEB_DETECTION\",\"maxResults\":1}]}]}"
+        
         var request = URLRequest(url: url!)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = body.data(using: String.Encoding.utf8)
         
         let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
             DispatchQueue.main.async {
